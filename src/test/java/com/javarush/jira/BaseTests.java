52ec6d33bee7788e83cdc1bfaa5d9db9196925da
alex <alexsemen666653@gmail.com>
2025-06-19 20:22:27 +0300

    Task 4 completed: the library Testcontainers is connected.

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -2,8 +2,30 @@ package com.javarush.jira;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 abstract class BaseTests {
+
+    @Container
+    static PostgreSQLContainer<?> postreSQL = new PostgreSQLContainer<>("postgres:latest");
+
+    // Добавление аннотаций @Testcontainers к классу и @Container к полю объявления контейнера позволит JUnit
+    // автоматически запустить контейнер перед выполнением тестов и автоматически уничтожить его после их выполнения.
+
+//    static {
+//        postreSQL.start();
+//    }
+//
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", postreSQL::getJdbcUrl);
+//        registry.add("spring.datasource.username", postreSQL::getUsername);
+//        registry.add("spring.datasource.password", postreSQL::getPassword);
+//    }
+
 }
